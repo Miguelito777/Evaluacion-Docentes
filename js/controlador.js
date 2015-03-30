85d427cb3e4164bf7480a9f74c15f11c7cb78935
Miguel_Angel_Menchu_Xoyon <mmenchuxoyon@localhost.localdomain>
2015-03-30 15:16:18 -0600

    Evaluacion Docentes listo para iniciar

---
+++ js/controlador.js
@@ -0,0 +1,5 @@
+window.onload = cargar;
+
+function cargar(){
+	alert("Hola mundo con todas la herramientas a trabajar el sistema de Evaluacion de Docentes");
+} 
---
+++ js/modelo.js
@@ -0,0 +1,3 @@
+function login(usuario, password){
+	
+}
